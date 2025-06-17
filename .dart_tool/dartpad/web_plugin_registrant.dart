d82d720d4b61ae59d651a5c0fa4a860acf0627c0
fasmartpaf <fasmartpaf@gmail.com>
2025-06-17 07:58:43 +0500

    story

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -12,6 +12,7 @@ import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:share_plus/src/share_plus_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -22,5 +23,6 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   ImagePickerPlugin.registerWith(registrar);
   SharePlusWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
